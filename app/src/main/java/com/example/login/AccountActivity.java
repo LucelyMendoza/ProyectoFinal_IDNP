67bedae7a9bd108443bb28122f1d6288aa67508b
LucelyMendoza <lmendoza@unsa.edu.pe>
2024-06-02 00:54:53 -0500

    Agregar archivos desde AndroidStudioProjects/Login

--- app/src/main/java/com/example/login/AccountActivity.java
+++ app/src/main/java/com/example/login/AccountActivity.java
@@ -4,22 +4,71 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
 
+import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
 
-import com.idnp2024a.loginsample.databinding.ActivityAccountBinding;
+import com.google.gson.Gson;
+import com.idnp2024a.loginsample.R;
 
 public class AccountActivity extends AppCompatActivity {
-
-    private ActivityAccountBinding binding;
+    public final static String ACCOUNT_RECORD = "ACCOUNT_RECORD";
+    public final static Integer ACCOUNT_ACEPTAR= 100;
+    public final static Integer ACCOUNT_CANCELAR= 200;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        binding = ActivityAccountBinding.inflate(getLayoutInflater());
-        setContentView(binding.getRoot());
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_account);
+        /*ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+            return insets;
+        });*/
+        Button btnAceptar = findViewById(R.id.btnAceptar);
+        Button btnCancelar = findViewById(R.id.btnCancel);
 
-        // Agregar OnClickListener al botón
+        EditText edtFirstName = findViewById(R.id.edtFirstname);
+        EditText edtLastName = findViewById(R.id.edtLastname);
+        EditText edtEmail = findViewById(R.id.edtEmail);
+        EditText edtPhone = findViewById(R.id.edtPhone);
+        EditText edtUsername2 = findViewById(R.id.edtUsername2);
+        EditText edtPassword2 = findViewById(R.id.edtPassword2);
+
+        btnAceptar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                AccountEntity accountEntity = new AccountEntity();
+                accountEntity.setFirstname(edtFirstName.getText().toString());
+                accountEntity.setLastname(edtLastName.getText().toString());
+                accountEntity.setEmail(edtEmail.getText().toString());
+                accountEntity.setPhone(edtPhone.getText().toString());
+                accountEntity.setUsername(edtUsername2.getText().toString());
+                accountEntity.setPassword(edtPassword2.getText().toString());
+
+                Gson gson = new Gson();
+                String accountJson = gson.toJson(accountEntity);
+
+                Intent data = new Intent();
+                data.putExtra(ACCOUNT_RECORD, accountJson);
+
+                setResult(ACCOUNT_ACEPTAR, data);
+                finish();
+
+            }
+        });
+        btnCancelar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                setResult(ACCOUNT_CANCELAR);
+                finish();
+            }
+        });
+    }
+}
+        /*// Agregar OnClickListener al botón
         binding.btnOK.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
@@ -44,7 +93,5 @@ public class AccountActivity extends AppCompatActivity {
                 Intent intent = new Intent(AccountActivity.this, LoginActivity.class);
                 intent.putExtra("accountEntity", accountEntity);
                 startActivity(intent);
-            }
-        });
-    }
-}
+            }*/
+
--- app/src/main/java/com/example/login/LoginActivity.java
+++ app/src/main/java/com/example/login/LoginActivity.java
@@ -10,12 +10,21 @@ import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Toast;
 
+import androidx.activity.result.ActivityResult;
+import androidx.activity.result.ActivityResultCallback;
+import androidx.activity.result.ActivityResultLauncher;
+import androidx.activity.result.contract.ActivityResultContracts;
 import androidx.appcompat.app.AppCompatActivity;
 
+import com.google.gson.Gson;
 import com.idnp2024a.loginsample.databinding.ActivityMainBinding;
 
 public class LoginActivity extends AppCompatActivity {
+    private static String TAB = "MainActivity";
     private ActivityMainBinding binding;
+    private AccountEntity accountEntity;
+    private String accountEntityString;
+    private ActivityResultLauncher<Intent> activityResultLauncher;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -40,21 +49,54 @@ public class LoginActivity extends AppCompatActivity {
         btnLogin.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                if (edtUsername.getText().toString().equals("Lucely") && edtPassword.getText().toString().equals("12345")) {
+                if (edtUsername.getText().toString().equals(accountEntity.getUsername()) && edtPassword.getText().toString().equals(accountEntity.getPassword())) {
                     // Autenticación exitosa
                     Toast.makeText(getApplicationContext(), "Autenticación exitosa", Toast.LENGTH_SHORT).show();
-                    String username = edtUsername.getText().toString();
-                    // Abrir HomeActivity y pasar el valor del campo de usuario
-                    Intent intent = new Intent(LoginActivity.this, HomeActivity.class);
-                    intent.putExtra("username", username);
+                    Log.d(TAG, "Bienvenido a mi app");
+
+                    Intent intent= new Intent(getApplicationContext(), HomeActivity.class);
+                    intent.putExtra("ACCOUNT", accountEntityString);
+
                     startActivity(intent);
                 } else {
                     // Autenticación fallidA
                     Toast.makeText(getApplicationContext(), "Error en la autenticación", Toast.LENGTH_SHORT).show();
+                    Log.d(TAG, "Error en la autenticación");
                 }
             }
-
         });
+
+        btnAddAccount.setOnClickListener(view1 -> {
+            Intent intent = new Intent(getApplicationContext(), AccountActivity.class);
+            activityResultLauncher.launch(intent);
+        });
+
+    activityResultLauncher = registerForActivityResult(
+            new ActivityResultContracts.StartActivityForResult(), new ActivityResultCallback<ActivityResult>() {
+                @Override
+                public void onActivityResult(ActivityResult activityResult) {
+                    Integer resultCode = activityResult.getResultCode();
+                    Log.d("LoginActivity", "resultCode: " + resultCode);
+                    if(resultCode==AccountActivity.ACCOUNT_ACEPTAR){
+                        Intent data = activityResult.getData();
+                        accountEntityString = data.getStringExtra(AccountActivity.ACCOUNT_RECORD);
+
+                        Gson gson = new Gson();
+                        accountEntity = gson.fromJson(accountEntityString, AccountEntity.class);
+
+                        String firstname = accountEntity.getFirstname();
+                        Toast.makeText(getApplicationContext(), "Nombre:" + firstname, Toast.LENGTH_SHORT).show();
+                        Log.d("LoginActivity", "Nombre: " + firstname);
+                    }
+                    else if(activityResult.getResultCode()==AccountActivity.ACCOUNT_CANCELAR){
+                        Toast.makeText(getApplicationContext(),"Cancelado", Toast.LENGTH_SHORT).show();
+                        Log.d("LoginActivity", "Cancelado");
+                    }
+                }
+            });
+    }
+}
+
 
 /*        btnRegistro.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -63,13 +105,7 @@ public class LoginActivity extends AppCompatActivity {
                 startActivity(intent);
             }
         });*/
-
-        btnAddAccount.setOnClickListener(view1 -> {
-            Intent intent = new Intent(getApplicationContext(),AccountActivity.class);
-            startActivity(intent);
-        });
-
-        Intent intent = getIntent();
+  /* Intent intent = getIntent();
         AccountEntity accountEntity = null;
         if (intent != null && intent.hasExtra("accountEntity")) {
             accountEntity = (AccountEntity) intent.getSerializableExtra("accountEntity");
@@ -81,5 +117,4 @@ public class LoginActivity extends AppCompatActivity {
             String password = accountEntity.getPassword();
         }
 
-    }
-}
+    }*/
